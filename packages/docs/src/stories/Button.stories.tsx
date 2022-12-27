37186b6986b443e26771a80a4f656f46fec000be
elias alexandre <elliasallex@gmail.com>
2022-12-27 19:38:17 -0300

    component: Button

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,52 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Button, ButtonProps } from "@cognu-ui/react";
+import { ArrowRight } from "phosphor-react";
+
+export default {
+  title: "Form/Button",
+  component: Button,
+
+  args: {
+    children: "Enviar",
+  },
+} as Meta<ButtonProps>;
+
+export const Primary: StoryObj<ButtonProps> = {};
+
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    children: "Próximo passo",
+    variant: "secondary",
+  },
+};
+
+export const WithIcon: StoryObj<ButtonProps> = {
+  args: {
+    children: (
+      <>
+        <span>Próximo passo</span>
+        <ArrowRight weight="bold" />
+      </>
+    ),
+  },
+};
+
+export const Tertiary: StoryObj<ButtonProps> = {
+  args: {
+    children: "Cancel",
+    variant: "tertiary",
+  },
+};
+
+export const Small: StoryObj<ButtonProps> = {
+  args: {
+    size: "sm",
+  },
+};
+
+export const Disabled: StoryObj<ButtonProps> = {
+  args: {
+    disabled: true,
+    size: "sm",
+  },
+};
