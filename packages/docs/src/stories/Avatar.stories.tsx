e6cf82909573f8ed54a7819051324395404da75b
elias alexandre <elliasallex@gmail.com>
2022-12-27 18:22:26 -0300

    component: Avatar

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Avatar, AvatarProps } from "@cognu-ui/react";
+
+export default {
+  title: "Data display/Avatar",
+  component: Avatar,
+
+  args: {
+    src: "https://github.com/eliasallex.png",
+    alt: "Elias Alexandre",
+  },
+} as Meta<AvatarProps>;
+
+export const Primary: StoryObj<AvatarProps> = {};
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+};
